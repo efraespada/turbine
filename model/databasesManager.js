cddabca1c7b3425f2eb6059d52ee1d931ca44d37
efraespada <efraespada@gmail.com>
2018-04-12 02:00:53 +0200

    inserting old functions for a single database

--- model/databasesManager.js
+++ model/databasesManager.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const setIn = require('set-in');
+const unset = require('unset');
 const JsonDB = require('node-json-db');
 const log = require('single-line-log').stdout;
 const RecursiveIterator = require('recursive-iterator');
@@ -16,6 +18,7 @@ function DatabasesManager(configuration) {
 
     this.databases = {};
     this.indexed = 0;
+    this.processed = 0;
     this.configuration = configuration;
 
     this.createDir = async function (dirPath) {
@@ -96,6 +99,64 @@ function DatabasesManager(configuration) {
         }
     };
 
+    this.getObject = function (database, value) {
+        this.processed++;
+        if (value.startsWith(SLASH) && value.length > SLASH.length) {
+            let branchs = value.split(SLASH);
+            let collections = Object.keys(this.databases[database]);
+            let objects = {
+                parts: []
+            };
+            for (let c in collections) {
+                let object = this.databases[database][collections[c]].data;
+                for (let b in branchs) {
+                    let branch = branchs[b];
+                    if (branch.length === 0) {
+                        continue;
+                    }
+                    if (object[branch] === undefined || object[branch] === null) {
+                        object[branch] = {};
+                    }
+                    object = object[branch];
+                }
+                objects.parts.push(object);
+            }
+            return this.mergeObjects(objects)
+        } else if (value.startsWith(SLASH) && value.length === SLASH.length) {
+            let collections = Object.keys(this.databases[database]);
+            let objects = {
+                parts: []
+            };
+            for (let c in collections) {
+                objects.parts.push(this.databases[database][collections[c]].data);
+            }
+            return this.mergeObjects(objects);
+        } else {
+            return null
+        }
+    };
+
+    this.mergeObjects = function(objects) {
+        let object = {};
+        if (objects.parts !== undefined && objects.parts.length > 1) {
+            for (let p in objects.parts) {
+                if (typeof objects.parts[p] === "object") {
+                    let keys = Object.keys(objects.parts[p]);
+                    for (let k in keys) {
+                        if (object[keys[k]] === undefined) {
+                            object[keys[k]] = objects.parts[p][keys[k]]
+                        }
+                    }
+                }
+            }
+            return object;
+        } else if (objects.parts !== undefined && objects.parts.length === 1) {
+            return objects.parts[0];
+        } else {
+            return null;
+        }
+    };
+
     this.reindexValues = function (params) {
         for (let {parent, node, key, path, deep} of new RecursiveIterator(params.data)) {
             if (typeof node !== "object") {
@@ -110,8 +171,114 @@ function DatabasesManager(configuration) {
         console.log("\n🎉")
     };
 
+    /**
+     * Returns an object from a instance for the given query and path (value)
+     * @param database
+     * @param value
+     * @param query
+     * @returns {*}
+     */
+    this.getObjectFromQuery = function (database, value, query) {
+        this.processed++;
+        if (query === undefined || query === null || JSON.stringify(query) === "{}" || value.indexOf("*") === -1) {
+            return null
+        } else if (value.startsWith(SLASH) && value.length > SLASH.length) {
+            let result = [];
+            let branchs = value.split(SLASH);
+            let collections = Object.keys(this.databases[database]);
+            for (let c in collections) {
+                let object = this.databases[database][collections[c]].data;
+                for (let b in branchs) {
+                    let branch = branchs[b];
+                    if (branch.length === 0) {
+                        continue;
+                    }
+                    if (branch === "*") {
+                        let keys = Object.keys(query);
+                        for (let k in keys) {
+                            let key = keys[k];
+                            if (this.databases[database][collections[c]].values[query[key]] !== undefined) {
+                                for (let p in this.databases[database][collections[c]].values[query[key]]) {
+                                    if (this.databases[database][collections[c]].values[query[key]][p].indexOf(value.replace(/\*/g, '')) > -1) {
+                                        let valid = this.databases[database][collections[c]].values[query[key]][p].replaceAll("/" + key, "");
+                                        result.push(action.getObject(database, valid));
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                    } else {
+                        if (object[branch] === undefined || object[branch] === null) {
+                            object[branch] = {};
+                        }
+                        object = object[branch];
+                    }
+                }
+            }
+            let res = [];
+            for (let obj in result) {
+                if (action.validateObject(result[obj], query)) {
+                    if (!action.containsObject(res, result[obj])) {
+                        res.push(result[obj])
+                    }
+                }
+            }
+            return res;
+        } else if (value.startsWith(SLASH) && value.length === SLASH.length) {
+            return this.getObject(database, value);
+        } else {
+            return null
+        }
+    };
+
+    /**
+     * Stores an object in the instance for the given type and path (value)
+     * @param database
+     * @param value ->  "/notifications/998476354
+     * @param object -> object to store
+     * @returns {*}
+     */
+    this.saveObject = function (database, value, object) {
+        this.processed++;
+        let store = null;
+        if (typeof object === "string") {
+            store = JSON.parse(object)
+        } else {
+            store = object;
+        }
+        // TODO update this
+        action.updateValDB(database, value, store);
+        if (store == null || JSON.stringify(store) === "{}") {
+            let collections = Object.keys(this.databases[database]);
+            for (let c in collections) {
+                this.databases[database][collections[c]].data = unset(this.databases[database][collections[c]].data, [value])
+            }
+        } else if (value.startsWith(SLASH) && value.length > SLASH.length) {
+            let branchsVal = value.split(SLASH);
+            let branchs = [];
+            for (let b in branchsVal) {
+                if (branchsVal[b].length > 0) {
+                    branchs.push(branchsVal[b]);
+                }
+            }
+            // TODO check if exist and if not, insert in best collection
+            if (database !== null && database === "paths") {
+                paths = setIn(paths, branchs, store);
+            } else {
+                data = setIn(data, branchs, store);
+            }
+
+        } else if (value.startsWith(SLASH) && value.length === SLASH.length) {
+            let collections = Object.keys(this.databases[database]);
+            for (let c in collections) {
+                // TODO insert on existing or insert in best collection
+                this.databases[database][collections[c]].data = store;
+            }
+        }
+    };
+
     this.getCollectionNumber = function (item) {
-        return item.replaceAll("col_", "").replaceAll(".json", "");
+        return this.getCollectionName(item.replaceAll("col_", ""));
     };
     this.getCollectionName = function (item) {
         return item.replaceAll(".json", "");
