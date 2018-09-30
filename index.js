e7d0c214a7fcb21a6327eafe8a93bb6324e13f92
Efra Espada <efraespada@gmail.com>
2018-09-30 04:26:42 +0200

    testing app

--- index.js
+++ index.js
@@ -1,186 +1,223 @@
-const forever =             require('forever-monitor');
-const rp =                  require('request-promise');
-const logjs =               require('logjsx');
-const fs =                  require('fs');
-const path =                require('path');
+const forever = require('forever-monitor');
+const rp = require('request-promise');
+const logjs = require('logjsx');
+const fs = require('fs');
+const path = require('path');
 const logger = new logjs();
 logger.init({
-    level: "DEBUG"
+  level: "DEBUG"
 });
 
 function Turbine(config) {
 
-    let o = this;
-    this.config = config;
-    this.turbine_ip = "http://localhost";
-    this.turbine_port = 7285;
-    this.databases = ["myDatabase"];
-    this.uid = "turbine";
-    this.log_dir = "";
-    this.debug = false;
+  let o = this;
+  this.config = config;
+  this.turbine_ip = "http://localhost";
+  this.turbine_port = 7285;
+  this.app_port = 7286;
+  this.databases = ["myDatabase"];
+  this.log_dir = "logs/";
+  this.debug = false;
 
-    if (this.config !== undefined) {
-        if (this.config.databases !== undefined && this.config.databases.length > 0) {
-            this.databases = this.config.databases;
-        }
-        if (this.config.turbine_port !== undefined && this.config.turbine_port > 0) {
-            this.turbine_port = this.config.turbine_port;
-        }
-        if (this.config.turbine_ip !== undefined) {
-            this.turbine_ip = this.config.turbine_ip;
-        }
-        if (this.config.debug !== undefined && this.config.debug) {
-            this.debug = this.config.debug;
-        }
-        if (this.config.log_dir !== undefined && this.config.log_dir) {
-            this.log_dir = this.config.log_dir;
-        }
+  if (this.config !== undefined) {
+    if (this.config.databases !== undefined && this.config.databases.length > 0) {
+      this.databases = this.config.databases;
     }
-
-    if (this.debug) {
-        logger.init({
-            level: "DEBUG"
-        });
+    if (this.config.turbine_port !== undefined && this.config.turbine_port > 0) {
+      this.turbine_port = this.config.turbine_port;
     }
+    if (this.config.turbine_ip !== undefined) {
+      this.turbine_ip = this.config.turbine_ip;
+    }
+    if (this.config.debug !== undefined && this.config.debug) {
+      this.debug = this.config.debug;
+    }
+    if (this.config.log_dir !== undefined && this.config.log_dir) {
+      this.log_dir = this.config.log_dir;
+    }
+    if (this.config.app_port !== undefined && this.config.app_port) {
+      this.app_port = this.config.app_port;
+    }
+  }
 
-    this.getRequest = function (url, data) {
-        return new Promise(function (resolve, reject) {
-            let options = {
-                uri: url,
-                qs: data,
-                json: true
-            };
-            rp(options)
-                .then(function (parsedBody) {
-                    resolve(parsedBody)
-                })
-                .catch(function (err) {
-                    reject(err)
-                });
+  if (this.debug) {
+    logger.init({
+      level: "DEBUG"
+    });
+  }
+
+  this.getRequest = function (url, data) {
+    return new Promise(function (resolve, reject) {
+      let options = {
+        uri: url,
+        qs: data,
+        json: true
+      };
+      rp(options)
+        .then(function (parsedBody) {
+          resolve(parsedBody)
+        })
+        .catch(function (err) {
+          reject(err)
         });
-    };
+    });
+  };
 
-    this.postRequest = function (url, data) {
-        return new Promise(function (resolve, reject) {
-            let options = {
-                method: 'POST',
-                uri: url,
-                body: data,
-                json: true
-            };
-            rp(options)
-                .then(function (parsedBody) {
-                    resolve(parsedBody)
-                })
-                .catch(function (err) {
-                    reject(err)
-                });
+  this.postRequest = function (url, data) {
+    return new Promise(function (resolve, reject) {
+      let options = {
+        method: 'POST',
+        uri: url,
+        body: data,
+        json: true
+      };
+      rp(options)
+        .then(function (parsedBody) {
+          resolve(parsedBody)
+        })
+        .catch(function (err) {
+          reject(err)
         });
+    });
+  };
+
+
+  /**
+   * Initializes Turbine process
+   */
+  this.server = function () {
+    o.startApp();
+    let process = "server";
+    let turbine_config = {
+      silent: false,
+      uid: process,
+      pidFile: "./" + process + ".pid",
+      max: 10,
+      killTree: true,
+
+      minUptime: 2000,
+      spinSleepTime: 1000,
+
+      sourceDir: __dirname,
+
+      args: ['DATABASES=' + this.databases, 'TURBINE_PORT=' + this.turbine_port, 'DEBUG=' + this.debug.toString()],
+
+      watch: false,
+      watchIgnoreDotFiles: null,
+      watchIgnorePatterns: null,
+      watchDirectory: null,
+
+
+      logFile: __dirname + "/" + o.log_dir + process + "/logFile.log",
+      outFile: __dirname + "/" + o.log_dir + process + "/outFile.log",
+      errFile: __dirname + "/" + o.log_dir + process + "/errFile.log"
     };
 
+    this.createDir(o.log_dir + process + "/").then(function () {
+      let child = forever.start('./turbine.js', turbine_config);
+    });
+  };
 
-    /**
-     * Initializes Turbine process
-     */
-    this.server = function () {
-        let turbine_config = {
-            silent: false,
-            uid: o.uid,
-            pidFile: "./" + o.uid + ".pid",
-            max: 10,
-            killTree: true,
+  this.startApp = async function () {
+    let process = "app";
+    let app_config = {
+      silent: false,
+      uid: process,
+      pidFile: "./" + process + ".pid",
+      max: 10,
+      killTree: true,
 
-            minUptime: 2000,
-            spinSleepTime: 1000,
+      minUptime: 2000,
+      spinSleepTime: 1000,
 
-            sourceDir: __dirname,
+      args: [],
 
-            args: ['DATABASES=' + this.databases, 'TURBINE_PORT=' + this.turbine_port, 'DEBUG=' + this.debug.toString()],
+      watch: false,
+      watchIgnoreDotFiles: null,
+      watchIgnorePatterns: null,
+      watchDirectory: null,
 
-            watch: false,
-            watchIgnoreDotFiles: null,
-            watchIgnorePatterns: null,
-            watchDirectory: null,
-
-
-            logFile: __dirname + "/logFile.log",
-            outFile: __dirname + "/outFile.log",
-            errFile: __dirname + "/errFile.log"
-        };
-
-        this.createDir(o.log_dir + o.uid + "/").then(function () {
-            let child = forever.start('./turbine.js', turbine_config);
-        });
+      logFile: __dirname + "/" + o.log_dir + process + "/logFile.log",
+      outFile: __dirname + "/" + o.log_dir + process + "/outFile.log",
+      errFile: __dirname + "/" + o.log_dir + process + "/errFile.log"
     };
 
-    /**
-     * Returns the object of the given path
-     * @param database -> myDatabase
-     * @param path
-     * @param mask
-     * @returns {Promise<*>}
-     */
-    this.get = async function(database, path, mask = {}) {
-        let data = {
-            method: "get",
-            database: database,
-            path: path,
-            interface: mask
-        };
-        return await this.getRequest(this.turbine_ip + ":" + this.turbine_port + "/", data)
-    };
+    this.createDir(o.log_dir + process + "/").then(function () {
+      let app = forever.start(["ng", "serve", "-o", "--port", o.app_port + ""], app_config);
+      app.on('start', function (code) {
+        logger.info("App started (" + o.app_port + ")");
+      });
+    });
+  };
 
-    /**
-     * Stores data in the given path. Removes if data is null or empty
-     * @param database -> myDatabase
-     * @param path
-     * @param value
-     * @returns {Promise<void>}
-     */
-    this.post = async function(database, path, value) {
-        let data = {
-            method: "post",
-            database: database,
-            path: path,
-            value: value
-        };
-        let response, err = await this.postRequest(this.turbine_ip + ":" + this.turbine_port + "/", data);
-        if (err) {
-            return err
-        }
-        return response
+  /**
+   * Returns the object of the given path
+   * @param database -> myDatabase
+   * @param path
+   * @param mask
+   * @returns {Promise<*>}
+   */
+  this.get = async function (database, path, mask = {}) {
+    let data = {
+      method: "get",
+      database: database,
+      path: path,
+      interface: mask
     };
+    return await this.getRequest(this.turbine_ip + ":" + this.turbine_port + "/", data)
+  };
 
-    /**
-     * Returns a list of objects that contains the given fields and values
-     * @param database -> myDatabase
-     * @param path -> /users/*
-     * @param query -> { name: "Mark" }
-     * @param interf -> { name: type("string) }
-     * @returns {Promise<*>}
-     */
-    this.query = async function(database, path, query, interf = {}) {
-        let data = {
-            method: "query",
-            database: database,
-            path: path,
-            query: query,
-            mask: interf,
-            token: ""
-        };
-        return await this.getRequest(this.turbine_ip + ":" + this.turbine_port + "/", data)
+  /**
+   * Stores data in the given path. Removes if data is null or empty
+   * @param database -> myDatabase
+   * @param path
+   * @param value
+   * @returns {Promise<void>}
+   */
+  this.post = async function (database, path, value) {
+    let data = {
+      method: "post",
+      database: database,
+      path: path,
+      value: value
     };
+    let response, err = await this.postRequest(this.turbine_ip + ":" + this.turbine_port + "/", data);
+    if (err) {
+      return err
+    }
+    return response
+  };
 
-    this.createDir = async function (dirPath) {
-        if (!await fs.existsSync(dirPath)) {
-            try {
-                await fs.mkdirSync(dirPath);
-            } catch (e) {
-                await this.createDir(path.dirname(dirPath));
-                await this.createDir(dirPath);
-            }
-        }
+  /**
+   * Returns a list of objects that contains the given fields and values
+   * @param database -> myDatabase
+   * @param path -> /users/*
+   * @param query -> { name: "Mark" }
+   * @param interf -> { name: type("string) }
+   * @returns {Promise<*>}
+   */
+  this.query = async function (database, path, query, interf = {}) {
+    let data = {
+      method: "query",
+      database: database,
+      path: path,
+      query: query,
+      mask: interf,
+      token: ""
     };
+    return await this.getRequest(this.turbine_ip + ":" + this.turbine_port + "/", data)
+  };
+
+  this.createDir = async function (dirPath) {
+    if (!await fs.existsSync(dirPath)) {
+      try {
+        await fs.mkdirSync(dirPath);
+      } catch (e) {
+        await this.createDir(path.dirname(dirPath));
+        await this.createDir(dirPath);
+      }
+    }
+  };
 
 }
 
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
